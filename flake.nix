726cfa79018b5587bfd363d191305112de7b805b
luanjaardim <luang.ajardim@gmail.com>
2024-09-05 12:59:35 -0300

    Feat: Adding code to find the non-trivial class

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Dev Shell for python using Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    py = pkgs.python3Packages;
+  in
+  {
+
+    devShells.${system}.default = pkgs.mkShell {
+
+      buildInputs = [
+        py.python
+        # Used to create a python enviroment, we can use pip normally with this
+        py.venvShellHook
+
+        # python packages that don't need to be controlled by pip
+        py.numpy
+        py.pandas
+        py.matplotlib
+        py.scikit-learn
+
+        # Any other package that my be needed in the shell can be added here too
+        pkgs.hello
+      ];
+
+      venvDir = "./.env"; # Path to the python envirotment
+
+      # As the shellHook is used to activate the Python Enviroment we enter our shell after it
+      postShellHook = "nu # Run nushell at the start of the dev shell";
+
+    };
+
+  };
+}
---
+++ non-trivial-class.py
@@ -0,0 +1,64 @@
+import qiskit as q
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+
+dataset = datasets.load_digits()
+data = dataset.data
+labels = dataset.target
+
+# Find the best representant of each class
+# Find the Non-Trivial class by comparing the best representant with the ones of the others classes
+
+print(data.shape)
+print(labels.shape)
+
+# Creating a dict that have an empty list to every label
+samples_by_class = dict(map(lambda x: (x, []), list(range(10))))
+
+for sample, label in zip(data, labels):
+    samples_by_class[label].append(sample)
+
+print(len(samples_by_class[0]))
+
+euc_dist = lambda sample1, sample2: sum((sample1-sample2)**2) ** 0.5
+
+for label, samples in samples_by_class.items():
+
+    representant = np.array([])
+    min_dist = 1e20             # init with with a big value
+
+    # the ith sample will be compared with every other jth sample
+    for i in range(len(samples)):
+        cur_dist = 0
+        for j in range(len(samples)):
+            if i == j: continue
+
+            # taking the euclidean distance between the ith samples and every other
+            # we want to find the sample that has the minimum distance to every other member of its class
+            cur_dist += euc_dist(samples[i], samples[j])
+
+        # get the representant and its dist sum
+        if min_dist > cur_dist:
+            representant, min_dist = samples[i], cur_dist
+
+    samples_by_class[label] = { 'samples': samples, 'representant': representant }
+
+# from PIL import Image
+# for c in samples_by_class.values():
+#     im = Image.fromarray(c['representant'].reshape((8, 8,)))
+#     im.show()
+
+# The non-trivial class is the one that has the shortest distance to every other class(easiest to classify as another class)
+non_trivial_label = -1
+min_dist = 1e20
+for i in range(10):
+    cur_dist = 0
+    for j in range(10):
+        cur_dist += euc_dist(samples_by_class[i]['representant'], samples_by_class[j]['representant']) if i != j else 0
+
+    if min_dist > cur_dist:
+        min_dist = cur_dist
+        non_trivial_label = i
+
+print('The non-trivial label is: ', non_trivial_label)
