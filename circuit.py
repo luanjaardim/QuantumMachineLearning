6d7bc2dba230be85c4c2e22a61a7df411022fba0
luanjaardim <luang.ajardim@gmail.com>
2024-09-20 16:09:19 -0300

    Wip: Random search optimization

--- circuit.py
+++ circuit.py
@@ -3,18 +3,21 @@ from pennylane import numpy as np
 
 from utils import *
 
-def generate_params(num_layers = 2, embedding_type = 'amplitude', prob_put_gate=0.6, sample_elim=True):
+def generate_params(num_layers = 2, embedding_type = 'amplitude', prob_put_gate=0.6, sample_elim=True, num_circuit=4):
     return {
             'embedding'     : embedding_type, # angle or amplitude
             'prob_put_gate' : prob_put_gate, # the probability to choose some gate to put at the position
             'num_layers'    : num_layers,
             'num_qubits'    : num_qubits[embedding_type],
+            'num_circuit'   : num_circuit,
             'max_num_gates' : num_layers * prob_put_gate * num_qubits[embedding_type],
-            'sample_elim'   : True,
-            'epochs'        : 30,
+            'sample_elim'   : sample_elim,
+            'batch_size'    : 5,
+            'epochs'        : 15,
     }
 
 def generate_weights(params, num=1):
+    # 1 is the maximum any of our gates can receive as parameters, some others can receive more.
     return np.random.randn(num, params['num_layers'], params['num_qubits'], 1, requires_grad=True)
 
 def create_model(params):
@@ -67,3 +70,56 @@ class QuantumCircuit:
 
     def variational_classifier(self, weights, bias, x):
         return self.circuit(weights, x) + bias
+
+    def train(self, weights_init, bias_init, X_train, X_test, y_train, y_test):
+
+        def cost(weights, bias, X, Y):
+            predictions = [self.variational_classifier(weights, bias, x) for x in X]
+            return square_loss(Y, predictions)
+
+        def accuracy(labels, predictions):
+            acc = sum(abs(l - p) < 1e-5 for l, p in zip(labels, predictions))
+            acc = acc / len(labels)
+            return acc
+
+        def square_loss(labels, predictions):
+            # We use a call to qml.math.stack to allow subtracting the arrays directly
+            return np.mean((labels - qml.math.stack(predictions)) ** 2)
+
+        opt = qml.NesterovMomentumOptimizer(0.01)
+
+        batch_size = self.params['batch_size']
+
+        # train the variational classifier
+        weights = weights_init
+        bias = bias_init
+        avg_acc = 0.0
+        for w, b in zip(weights, bias):
+            best_acc = -1
+            for it in range(self.params['epochs']):
+                # Update the weights by one optimizer step
+                batch_index = np.random.randint(0, train_size * len(X_train), (batch_size,))
+                feats_train_batch = X_train[batch_index]
+                Y_train_batch = y_train[batch_index]
+
+                w, b, _, _ = opt.step(cost, w, b, feats_train_batch, Y_train_batch)
+
+                # Compute predictions on train and validation set
+                predictions_train = np.sign(self.variational_classifier(w, b, X_train))
+                predictions_val = np.sign(self.variational_classifier(w, b, X_test))
+
+                # Compute accuracy on train and validation set
+                acc_train = accuracy(y_train, predictions_train)
+                acc_val = accuracy(y_test, predictions_val)
+
+                if best_acc < acc_val:
+                    best_acc = acc_val
+
+                if (it + 1) % 2 == 0:
+                    _cost = cost(w, b, X_train, y_train)
+                    print(
+                        f"Iter: {it + 1:5d} | Cost: {_cost:0.7f} | "
+                        f"Acc train: {acc_train:0.7f} | Acc validation: {acc_val:0.7f}"
+                    )
+            avg_acc += best_acc
+        return avg_acc / len(weights_init)
--- main.py
+++ main.py
@@ -12,55 +12,23 @@ params = generate_params()
 
 X_train, X_test, y_train, y_test = import_database(params)
 
-c = QuantumCircuit(params)
-
-drawer = qml.draw(c.circuit)
-
-# 1 is the maximum any of our gates can receive as parameters, some others can receive more.
-weights_init = generate_weights(params, num=10)
+weights_init = generate_weights(params, num=5)
 bias_init = np.zeros(10, requires_grad=True)
-print(drawer(weights_init[0], X_train))
 
-def cost(weights, bias, X, Y):
-    predictions = [c.variational_classifier(weights, bias, x) for x in X]
-    return square_loss(Y, predictions)
+circuits = []
 
-def accuracy(labels, predictions):
-    acc = sum(abs(l - p) < 1e-5 for l, p in zip(labels, predictions))
-    acc = acc / len(labels)
-    return acc
+for i in range(params['num_circuit']):
 
-def square_loss(labels, predictions):
-    # We use a call to qml.math.stack to allow subtracting the arrays directly
-    return np.mean((labels - qml.math.stack(predictions)) ** 2)
+    c = QuantumCircuit(params)
 
-opt = qml.NesterovMomentumOptimizer(0.01)
+    drawer = qml.draw(c.circuit)
 
-batch_size = 5
+    print(drawer(weights_init[0], X_train))
+    # TODO: return average accuracy of the circuit
+    avg_acc = c.train(weights_init, bias_init, X_train, X_test, y_train, y_test)
+    print('Circuit\'s average best accuracy: ', avg_acc)
 
-# train the variational classifier
-weights = weights_init
-bias = bias_init
-for w, b in zip(weights, bias):
-    for it in range(params['epochs']):
-        # Update the weights by one optimizer step
-        batch_index = np.random.randint(0, train_size * len(X_train), (batch_size,))
-        feats_train_batch = X_train[batch_index]
-        Y_train_batch = y_train[batch_index]
+    circuits.append((c, avg_acc))
 
-        w, b, _, _ = opt.step(cost, w, b, feats_train_batch, Y_train_batch)
-
-        # Compute predictions on train and validation set
-        predictions_train = np.sign(c.variational_classifier(w, b, X_train))
-        predictions_val = np.sign(c.variational_classifier(w, b, X_test))
-
-        # Compute accuracy on train and validation set
-        acc_train = accuracy(y_train, predictions_train)
-        acc_val = accuracy(y_test, predictions_val)
-
-        if (it + 1) % 2 == 0:
-            _cost = cost(w, b, X_train, y_train)
-            print(
-                f"Iter: {it + 1:5d} | Cost: {_cost:0.7f} | "
-                f"Acc train: {acc_train:0.7f} | Acc validation: {acc_val:0.7f}"
-            )
+# TODO: rank of trained circuits
+print(circuits)
